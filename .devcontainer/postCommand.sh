b936bab0758a5ef09281b672c3f96cb0ad140e94
azuki774 <azuki774s@gmail.com>
2025-12-04 15:31:48 +0000

    install go devcontainer

--- .devcontainer/postCommand.sh
+++ .devcontainer/postCommand.sh
@@ -5,4 +5,3 @@ export PGPASSWORD=postgres
 ## マイグレーション
 psql -h localhost -U postgres -c "CREATE DATABASE nostar;"
 psql -h localhost -U postgres -d nostar -f scripts/migrations/001_init.sql
-psql -h localhost -U postgres -d nostar -c "SELECT * FROM events;"
